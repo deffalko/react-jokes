7eb2e0033dbb3746720f7a3ea8e59ed961ba7ce3
артур <gumarovartur059@gmail.com>
2025-04-10 19:42:25 +0300

    basket css

--- src/Components/BasketList.jsx
+++ src/Components/BasketList.jsx
@@ -2,18 +2,20 @@ import { BasketItem } from "./BasketItem";
 
 function BasketList({ basket, handleBasketShow, removeFromBasket }) {
   return (
-    <div className="basket">
-      {basket.length === 0 ? (
-        <p>Корзина пуста</p>
-      ) : (
-        basket.map((joke) => (
-          <BasketItem
-            key={joke.id}
-            joke={joke}
-            removeFromBasket={removeFromBasket}
-          />
-        ))
-      )}
+    <div className="basket-box">
+      <div className="basket">
+        {basket.length === 0 ? (
+          <p className="basket-tytle">Корзина пуста</p>
+        ) : (
+          basket.map((joke) => (
+            <BasketItem
+              key={joke.id}
+              joke={joke}
+              removeFromBasket={removeFromBasket}
+            />
+          ))
+        )}
+      </div>
       <i className="material-icons basket-close" onClick={handleBasketShow}>
         close
       </i>
